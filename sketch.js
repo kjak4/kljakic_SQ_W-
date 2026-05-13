12fe75a3547160f6d436500734a25e36b47ddccf
kjak4 <kljakic@uwaterloo.ca>
2026-05-13 17:52:01 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -44,14 +44,14 @@ function setup() {
   // ----------------------------------------------------------
   // SECTION 3: DRAWING SHAPES 
   // ----------------------------------------------------------
-  let originX = 400; 
-  let originY = 185; // 
+  let originX = 600; 
+  let originY = 200; // 
   stroke(255);
   strokeWeight(9);
   fill(9);
 
-  circle(originX-300, originY, 140);
-  circle(originX-300, originY+150, 170);
+  circle(originX, originY, 140);
+  circle(originX, originY+150, 170);
 
 }
 
