03ba3f956eb0b5198cb1c0507cadf94e9bacb21b
kjak4 <kljakic@uwaterloo.ca>
2026-05-13 17:43:28 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -44,14 +44,14 @@ function setup() {
   // ----------------------------------------------------------
   // SECTION 3: DRAWING SHAPES 
   // ----------------------------------------------------------
-  let originX = 600; // left edge of the shape controller
+  let originX = 500; // left edge of the shape controller
   let originY = 185; // top edge of the shape controller
   stroke(255);
   strokeWeight(9);
   fill(9);
 
-  circle(originX, originY, 140);
-  circle(originX, originY+150, 170);
+  circle(originX+50, originY, 140);
+  circle(originX+50, originY+150, 170);
 
 }
 
